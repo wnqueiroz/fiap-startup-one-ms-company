c40797c7e37b05909328efef2aace2512c0b7e18
William Queiroz <wnqueiroz@outlook.com>
2021-02-15 17:26:33 -0300

    test: added unit tests to services.controller.ts

---
+++ src/services/services.controller.spec.ts
@@ -0,0 +1,158 @@
+import { Test } from '@nestjs/testing';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+import { ServicesController } from './services.controller';
+import { ServicesService } from './services.service';
+import { ServiceEntity } from './service.entity';
+import { ServicePeriodsEntity } from './service-periods.entity';
+import { CompanyEntity } from '../companies/company.entity';
+
+import { ServiceDTO } from './dtos/service.dto';
+import { UpdateServiceDTO } from './dtos/update-service.dto';
+import { CreateServicePeriodDTO } from './dtos/create-service-period.dto';
+import { ServicePeriodDTO } from './dtos/service-period.dto';
+
+import { KAFKA_CLIENTS, KAFKA_TOPICS } from '../contants';
+
+describe('ServicesController', () => {
+  let serviceEntity: ServiceEntity;
+  let servicesService: ServicesService;
+  let servicesController: ServicesController;
+
+  const id = 'foo';
+
+  const mockToPromiseClientKafka = jest.fn(async () => null);
+  const mockToEmitClientKafka = jest.fn(() => ({
+    toPromise: mockToPromiseClientKafka,
+  }));
+
+  beforeEach(async () => {
+    const MockClientKafka = {
+      emit: mockToEmitClientKafka,
+    };
+
+    const moduleRef = await Test.createTestingModule({
+      controllers: [ServicesController],
+      providers: [
+        ServicesService,
+        {
+          provide: getRepositoryToken(ServiceEntity),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(ServicePeriodsEntity),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(CompanyEntity),
+          useClass: Repository,
+        },
+        {
+          provide: KAFKA_CLIENTS.SERVICES_SERVICE,
+          useFactory: () => MockClientKafka,
+        },
+      ],
+    }).compile();
+
+    servicesService = moduleRef.get<ServicesService>(ServicesService);
+    servicesController = moduleRef.get<ServicesController>(ServicesController);
+
+    serviceEntity = new ServiceEntity();
+  });
+
+  describe('getOne', () => {
+    it('should return a service by ID', async () => {
+      jest
+        .spyOn(servicesService, 'getOne')
+        .mockImplementation(async () => serviceEntity);
+
+      expect(
+        await servicesController.getOne({
+          id,
+        }),
+      ).toStrictEqual(new ServiceDTO(serviceEntity));
+    });
+  });
+
+  describe('updateOne', () => {
+    it('should update a service by ID', async () => {
+      const updateServiceDto: UpdateServiceDTO = {
+        name: 'Foo',
+      };
+
+      jest
+        .spyOn(servicesService, 'updateOne')
+        .mockImplementation(async () => serviceEntity);
+
+      expect(
+        await servicesController.updateOne(
+          {
+            id,
+          },
+          updateServiceDto,
+        ),
+      ).toStrictEqual(new ServiceDTO(serviceEntity));
+      expect(servicesService.updateOne).toBeCalledWith(id, updateServiceDto);
+
+      expect(
+        mockToEmitClientKafka,
+      ).toBeCalledWith(KAFKA_TOPICS.SERVICES_UPDATED, { ...serviceEntity });
+      expect(mockToPromiseClientKafka).toBeCalled();
+    });
+  });
+
+  describe('deleteOne', () => {
+    it('should delete a service by ID', async () => {
+      jest
+        .spyOn(servicesService, 'deleteOne')
+        .mockImplementation(async () => serviceEntity);
+
+      expect(
+        await servicesController.deleteOne({
+          id,
+        }),
+      ).toStrictEqual(new ServiceDTO(serviceEntity));
+      expect(servicesService.deleteOne).toBeCalledWith(id);
+      expect(
+        mockToEmitClientKafka,
+      ).toBeCalledWith(KAFKA_TOPICS.SERVICES_DELETED, { ...serviceEntity });
+      expect(mockToPromiseClientKafka).toBeCalled();
+    });
+  });
+
+  describe('createPeriods', () => {
+    it('should create a service period for the service by ID', async () => {
+      const createServicePeriodDTO: CreateServicePeriodDTO = {
+        startTime: '10:30',
+        endTime: '11:30',
+      };
+
+      const servicePeriodsEntity = new ServicePeriodsEntity();
+
+      jest
+        .spyOn(servicesService, 'createPeriods')
+        .mockImplementation(async () => servicePeriodsEntity);
+
+      expect(
+        await servicesController.createPeriods(
+          {
+            id,
+          },
+          createServicePeriodDTO,
+        ),
+      ).toStrictEqual(new ServicePeriodDTO(serviceEntity));
+      expect(servicesService.createPeriods).toBeCalledWith(
+        id,
+        createServicePeriodDTO,
+      );
+      expect(mockToEmitClientKafka).toBeCalledWith(
+        KAFKA_TOPICS.SERVICE_PERIODS_CREATED,
+        {
+          ...servicePeriodsEntity,
+        },
+      );
+      expect(mockToPromiseClientKafka).toBeCalled();
+    });
+  });
+});
